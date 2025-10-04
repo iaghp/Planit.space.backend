32f43d57a22b6106f69a9065aa5926f30128a173
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-10-04 09:45:42 -0400

    init db connection

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require('express')
+import express from 'express';
+import 'dotenv/config'
+
+import db from './db/conn.js';
+
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/test', (req, res) => {
+  db.insertOne('users', {
+    name: "Bob",
+    schedule: "3424324234fsdfjisd"
+  })
   res.send('Hello World!')
 })
 
