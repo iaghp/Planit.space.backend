5f6744ff72a03b76744a0b5da8c3a5f05058934e
MadisonMajarais <madison.majarais@gmail.com>
2025-10-04 16:58:54 -0400

    initial commit for adding gemini

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import 'dotenv/config'
 
 import db from './db/conn.js';
+import gemini from './services/gemini.js';
 
 
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,6 +22,19 @@ app.get('/test', (req, res) => {
   })
   res.send('Hello World!')
 })
+
+app.get('/generate', async (req, res) => {
+  let newPlan = {
+    name: req.body.name,
+    context: req.body.context,
+    deadline: req.body.deadline,
+    currentTime: new Date().toISOString()
+  }
+  console.log(newPlan)
+  const response = await gemini.generatePlan(newPlan);
+  res.send(response)
+}
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
